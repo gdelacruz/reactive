7b2d556df0d9f6568159c3c192f79de4d777779f
German de la Cruz <german.delacruz@gmail.com>
2013-11-28 16:21:13 -0300

    arreglo para segunda entrega

--- suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
+++ suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
@@ -99,8 +99,8 @@ trait WikipediaApi {
      *
      * Observable(Success(1), Succeess(1), Succeess(1), Succeess(2), Succeess(2), Succeess(2), Succeess(3), Succeess(3), Succeess(3))
      */
-    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = obs.flatMap(requestMethod(_)).recovered
-
+    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = obs.map(requestMethod(_).recovered).concat
+    
   }
 
 }
